7a517d1e6246d7491cb83adaaa83dd3eb6cf1875
vrxfile <vrxfile@mail.ru>
2017-09-28 17:21:26 +0300

    Added libraries and reading sensors

--- Test_sketch_easy_shield_pump.ino
+++ Test_sketch_easy_shield_pump.ino
@@ -52,6 +52,17 @@ void setup() {
   pinMode(PUMP, OUTPUT);
   digitalWrite(PUMP, LOW);
 
+  // Инициализация выходов светодиодов
+  pinMode(R_LED, OUTPUT);
+  pinMode(G_LED, OUTPUT);
+  pinMode(B_LED, OUTPUT);
+  pinMode(RED_LED, OUTPUT);
+  pinMode(BLUE_LED, OUTPUT);
+  digitalWrite(R_LED, LOW);
+  digitalWrite(G_LED, LOW);
+  digitalWrite(B_LED, LOW);
+  digitalWrite(RED_LED, LOW);
+  digitalWrite(BLUE_LED, LOW);
 }
 
 void loop() {
@@ -61,15 +72,19 @@ void loop() {
 void readSensorData() {
   float air_hum = dht1.readHumidity();                // Считывание влажности воздуха
   float air_temp = dht1.readTemperature();            // Считывание температуры воздуха
-  Serial.print("Air temperature = ");
+  Serial.print("Air temperature 1 = ");
   Serial.println(air_temp);
   Serial.print("Air humidity = ");
   Serial.println(air_hum);
 
-  ds_sensor1.requestTemperatures();                   // Считывание температуры воздуха
+  ds_sensor1.requestTemperatures();                   // Считывание температуры с DS18B20
   float soil_temp = ds_sensor1.getTempCByIndex(0);
   Serial.print("Soil temperature = ");
   Serial.println(soil_temp);
+
+  float air_temp_lm35 = analogRead(LM35_SENSOR) / 1023.0 * 500.0;    // Считывание температуры с LM35
+  Serial.print("Air temperature 2 = ");
+  Serial.println(air_temp_lm35);
 
   float light = analogRead(LIGHT_SENSOR) / 1023.0 * 100.0;    // Считывание датчика света
   float soil_hum = analogRead(SOIL_SENSOR) / 1023.0 * 100.0;  // Считывание влажности почвы
@@ -77,5 +92,9 @@ void readSensorData() {
   Serial.println(soil_hum);
   Serial.print("Light = ");
   Serial.println(light);
+
+  float resistance = analogRead(RESISTOR) / 1023.0 * 10.0;    // Считывание показаний переменного резистора
+  Serial.print("Resistance = ");
+  Serial.println(resistance);
 }
 
